e89ab26b4825f083825f9fd45c63f1f9e384f25f
python-codes <1821199691@qq.com>
2020-03-28 13:17:33 +0800

    鼠标绑定

---
+++ project4/app.js
@@ -0,0 +1,22 @@
+new Vue({
+    el:"#vue-app",
+    data:{
+        age:30,
+        X:0,
+        Y:0,
+    },
+    methods: {
+        add:function(n){
+            this.age+=n;
+        },
+        subtract:function(n){
+            this.age-=n;
+        },
+        updateXY:function(event) {
+            // console.log(event);
+            this.X = event.offsetX;
+            this.Y = event.offsetY;
+        },
+    },
+});
+
