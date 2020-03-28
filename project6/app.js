ddeba2c36e7fe7c32c45aaabbaeceac7283cc7f0
python-codes <1821199691@qq.com>
2020-03-28 13:52:09 +0800

    键盘事件及其修饰符

---
+++ project6/app.js
@@ -0,0 +1,16 @@
+new Vue({
+    el:"#vue-app",
+    data:{
+    },
+    methods: {
+        logName:function(){
+            // alert("正在输入名字");
+            console.log("正在输入名字");
+        },
+        logAge:function(){
+            // alert("正在输入名字");
+            console.log("正在输入年龄");
+        }
+    },
+});
+
