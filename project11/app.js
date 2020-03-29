4b87ed56dd660fbe9d133ec378178c56dfedae23
python-codes <1821199691@qq.com>
2020-03-29 17:45:24 +0800

    循环

---
+++ project11/app.js
@@ -0,0 +1,18 @@
+new Vue({
+    el: "#vue-app",
+    data: {
+        characters:["mario","Luffy","bucky"],
+        users:[
+            {name:"henry",age:30},
+            {name:"Emily",age:20},
+            {name:"tome",age:33}
+        ]
+    },
+    methods: {
+        
+    },
+    computed:{
+        
+    }
+});
+
