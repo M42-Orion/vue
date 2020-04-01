6381c86a5edd5ec131177a6ae053c62d82989a1c
python-codes <1821199691@qq.com>
2020-04-01 15:33:06 +0800

    CSS的作用域

--- my-project/src/App.vue
+++ my-project/src/App.vue
@@ -1,29 +1,32 @@
 <template>
   <div id="app">
-     <h1>{{title}}</h1>
-     <users></users>
+    <h1>{{title}}</h1>
+    <users></users>
   </div>
 </template>
 
 <script>
 // 行为，处理逻辑
 // 局部注册组件
-import Users from "./components/Users"
+import Users from "./components/Users";
 
 export default {
-  name: 'app',
+  name: "app",
   data() {
     return {
-      title:"这是我的第一个vue脚手架项目！"
-    }
+      title: "这是我的第一个vue脚手架项目！"
+    };
   },
-  components:{
+  components: {
     // 起名字不能跟系统标签冲突
     Users
   }
-}
+};
 </script>
 
-<style>
-
+<style scoped>
+/* scoped标明样式的作用域 */
+h1 {
+  color: purple;
+}
 </style>
