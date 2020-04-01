b0f5ee5085651f2973f49099a3252ed115a471a3
python-codes <1821199691@qq.com>
2020-04-01 16:00:02 +0800

    实战demo

--- my-project/src/App.vue
+++ my-project/src/App.vue
@@ -1,14 +1,18 @@
 <template>
   <div id="app">
+    <app-header></app-header>
     <h1>{{title}}</h1>
     <users></users>
+    <app-footer></app-footer>
   </div>
 </template>
 
 <script>
 // 行为，处理逻辑
 // 局部注册组件
-import Users from "./components/Users";
+import Users from "./components/Users"
+import Header from "./components/Header"
+import Footer from "./components/Footer"
 
 export default {
   name: "app",
@@ -19,7 +23,9 @@ export default {
   },
   components: {
     // 起名字不能跟系统标签冲突
-    Users
+    "users":Users,
+    "app-header":Header,
+    "app-footer":Footer
   }
 };
 </script>
--- my-project/src/components/Users.vue
+++ my-project/src/components/Users.vue
@@ -1,24 +1,54 @@
 <template>
-    <div class = "users">
-        <ul>
-            <li v-for="user in users">
-                {{user}}
-            </li>
-        </ul>
-    </div>
+  <div class="users">
+    <h1>Hello users</h1>
+    <ul>
+      <li v-for="user in users" >
+        <h2 v-on:click="user.show = !user.show">{{user.name}}</h2>
+        <h3 v-show="user.show">{{user.position}}</h3>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
 export default {
-    name:'users',
-    data() {
-        return {
-            users:["Henry","Bucky","Emily"]
-        }
-    },
-}
+  name: "users",
+  data() {
+    return {
+      users: [
+        { name: "Henry", position: "web开发", show: false },
+        { name: "Bucky", position: "web开发", show: false },
+        { name: "Emily", position: "web开发", show: true }
+      ]
+    };
+  }
+};
 </script>
 
-<style slot-scope = "">
-    
+<style scoped>
+/* scoped标明样式的作用域 */
+h1 {
+  color: green;
+}
+.uesers{
+    width: 100%;
+    max-width: 1200px;
+    margin: 40px auto;
+    padding: 0 20px;
+    box-sizing: border-box;
+}
+ul{
+    display: flex;
+    flex-wrap: wrap;
+    list-style-type: none;
+    padding: 0;
+}
+li{
+    flex-grow: 1;
+    flex-basis: 200px;
+    text-align: center;
+    padding: 30px;
+    border: 1px solid #222;
+}
 </style>
+    
